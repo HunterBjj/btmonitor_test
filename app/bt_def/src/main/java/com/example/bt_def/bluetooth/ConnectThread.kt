f4f9cd305c641ee3e56358501234414bc85e10e9
NIIPP_LBR_BTMN <a.urmanow@mail.ru>
2024-03-04 12:21:34 +0700

    add new file controller and thread ConnectThread

---
+++ app/bt_def/src/main/java/com/example/bt_def/bluetooth/ConnectThread.kt
@@ -0,0 +1,37 @@
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import java.io.IOException
+import java.util.*
+
+class ConnectThread(device: BluetoothDevice, val listener: BluetoothController.Listener) : Thread() {
+    private val uuid = "00001101-0000-1000-8000-00805F9B34FB"
+    private var mSocket: BluetoothSocket? = null
+    init {
+        try {
+            mSocket = device.createRfcommSocketToServiceRecord(UUID.fromString(uuid))
+        } catch (e: IOException){
+
+        } catch (se: SecurityException){
+
+        }
+    }
+
+    override fun run() {
+        try {
+            mSocket?.connect()
+            listener.onReceive(BluetoothController.BLUETOOTH_CONNECTED)
+        } catch (e: IOException){
+            listener.onReceive(BluetoothController.BLUETOOTH_NO_CONNECTED)
+        } catch (se: SecurityException){
+
+        }
+    }
+
+    fun closeConnection(){
+        try {
+            mSocket?.close()
+        } catch (e: IOException){
+
+        }
+    }
+}
